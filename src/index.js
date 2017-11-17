44d7ad5662b9c4dcff83ff67c47b4d9d7e0badbf
Ali G <ali.glenesk@gmail.com>
2017-11-16 17:51:56 -1000

    Working on react

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Render me!</h1>, document.getElementById('root'));
+const heading = (<div className="big">
+        <h1>Learning ALL the things!</h1>
+    </div>
+    );
+
+const thelist = (
+  <ul>
+    <li>This</li>
+    <li>is</li>
+    <li>a</li>
+    <li>list</li>
+  </ul>
+  );
+
+ReactDOM.render(
+    heading, 
+    document.getElementById('root')
+    );
+
+// ReactDOM.render(thelist, document.getElementById('thingtwo'));

51c7aaee67cdb55b7a55fa98c77ceb87cb6a1efd
Ali G <ali.glenesk@gmail.com>
2017-11-16 18:38:58 -1000

    Puppies and kittens

--- src/index.js
+++ src/index.js
@@ -18,8 +18,7 @@ const thelist = (
 
 const puppy = require('random-puppy');
 
-puppy()
-    .then(url => {
+puppy().then(url => {
         console.log(url);
     })
 
@@ -29,3 +28,19 @@ ReactDOM.render(
     );
 
 // ReactDOM.render(thelist, document.getElementById('thingtwo'));
+
+function makeDoggy(e) {
+  // Call this extremely useful function on an <img>.
+  // The <img> will become a picture of a doggy.
+  e.target.setAttribute('src', 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-puppy.jpeg');
+  e.target.setAttribute('alt', 'doggy');
+}
+
+const kitty = (
+    <img onClick={makeDoggy}
+        src="https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-kitty.jpg" 
+        alt="kitty"
+        height="400" />
+);
+
+ReactDOM.render(kitty, document.getElementById('root'));
